bd6e21a03f0eddd140277be01bae87f5c17a822d
Bas Pennings <bas.pennings@asml.com>
2018-10-25 17:37:22 +0200

    Sandboxing

--- pollen.rkt
+++ pollen.rkt
@@ -5,7 +5,7 @@
 (provide $ $$ root)
 
 (define (root . elements)
-  (txexpr 'root empty (decode-elements elements #:txexpr-elements-proc decode-paragraphs)))
+  [txexpr 'root empty (decode-elements elements #:txexpr-elements-proc decode-paragraphs)])
 
 (define ($ . xs)
   `(mathjax ,(apply string-append `("$" ,@xs "$"))))
@@ -15,5 +15,4 @@
 
 (module setup racket
   [provide (all-defined-out)]
-  [define omitted-path? (lambda (path) 
-    (string-contains? (path->string path) ".vscode"))])
+  [define omitted-path? (lambda (path) (string-contains? (path->string path) ".vscode"))])
